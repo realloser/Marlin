ee15a85d01e4cd35a552a3c91f9520ae274d2712
Dan Lipsitt <dan@typeamachines.com>
2015-07-14 14:08:07 -0700

    Turn off heated bed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -17,7 +17,7 @@
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION "1.2.0"
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(Type A Machines v" STRING_VERSION ")" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Type A Machines v" STRING_VERSION "no heated bed)" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
@@ -137,7 +137,7 @@
 #define TEMP_SENSOR_0 1
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
-#define TEMP_SENSOR_BED 1
+#define TEMP_SENSOR_BED 0
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
