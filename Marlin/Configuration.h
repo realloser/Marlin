bc85a58210010a6b655d7cf5e42048e5c7ce9f7a
Martyn Messerli <realloser@gmx.ch>
2017-07-18 19:48:21 +0200

    Enable heated bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -15,7 +15,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_VERSION "1.2.1"
+#define STRING_VERSION "1.2.1 Martyn"
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
 #define STRING_CONFIG_H_AUTHOR "(Type A Machines v" STRING_VERSION ")" // Who made the changes.
 
@@ -219,7 +219,7 @@
 // If your configuration is significantly different than this and you don't understand the issues involved, you probably
 // shouldn't use bed PID until someone else verifies your hardware works.
 // If this is enabled, find your own PID constants below.
-//#define PIDTEMPBED
+#define PIDTEMPBED
 //
 //#define BED_LIMIT_SWITCHING
 
